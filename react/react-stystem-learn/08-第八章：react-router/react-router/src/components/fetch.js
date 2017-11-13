ae8f9e2eba1d51a0f19c3f89829a1dbd65ee6d0a
poetries <jingguanliuye@gmail.com>
2017-11-14 02:22:33 +0800

    update

---
+++ react/react-stystem-learn/08-第八章：react-router/react-router/src/components/fetch.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Demo extends React.Component {
+    getData(){
+        
+    }
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Demo;
